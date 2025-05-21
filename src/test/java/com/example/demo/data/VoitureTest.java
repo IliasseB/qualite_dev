703bd58a4692c01841e8bf75124bc5ba681ac584
BELLOUCH Iliasse <12201699@q10408.iutv.univ-paris13.fr>
2025-05-21 10:37:52 +0200

    first commit

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -10,8 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoiture() {
+        // Création d'une voiture avec le constructeur avec paramètres
+        Voiture voiture = new Voiture("Renault", 20000);
+
+        // Vérification des valeurs initiales
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(20000, voiture.getPrix());
+        assertEquals(0, voiture.getId());
     }
 
 }
