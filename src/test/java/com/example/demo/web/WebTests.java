8f46a03993ac4c769449dbead458bdf99f3c1163
BELLOUCH Iliasse <12201699@q10408.iutv.univ-paris13.fr>
2025-05-21 11:56:16 +0200

    newcarservice

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -1,10 +1,9 @@
 package com.example.demo.web;
 
 import com.example.demo.data.Voiture;
-import com.example.demo.service.Echantillon;
 import com.example.demo.service.StatistiqueImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,19 +11,32 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class WebTests {
-
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
-
+public class WebTests {
     @Autowired
-    MockMvc mockMvc;
-
+    private MockMvc mockMvc;
+    
+    @MockBean
+    private StatistiqueImpl statistique;
+    
+    @Test
+    public void testAjoutVoiture() throws Exception {
+        // Création de l'objet et conversion en JSON
+        Voiture voiture = new Voiture("ferrari", 5000);
+        String voitureJson = new ObjectMapper().writeValueAsString(voiture);
+        
+        // Test de l'appel REST
+        mockMvc.perform(post("/voiture")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(voitureJson))
+                .andExpect(status().isOk());
+        
+        // Vérification du service
+        verify(statistique).ajouter(voiture);
+    }
 }
